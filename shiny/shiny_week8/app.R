813948baadac15d557c6586f5e52dd027b997f4b
Ben Elliott <ellio747@umn.edu>
2026-03-19 17:03:39 -0500

    Plain text explainations added to Rmd Renamed skinny file

--- shiny/shiny_week8/app.R
+++ shiny/shiny_week8/app.R
@@ -1,34 +1,57 @@
-
 library(shiny)
+library(ggplot2)
+library(dplyr)
+
 ui <- fluidPage(
     titlePanel("Shiny Week 8"),
     sidebarLayout(
+        #sidebarPanels can be used to stack each of the inputs on the lefthand side
         sidebarPanel(
-            selectInput("gender",
-                        "Select Gender",
-                        choices = c("Male", "Female", "All"),
-                        selected = "All"),
-            selectInput("se_show",
-                        "Error Bar Visible",
-                        choices = c("Display Error Band", "Suprress Error Band"),
-                        selected = "Display Error Band"),
-            checkboxInput("by_date",
-                        "Include Participants Prior to July 1, 2017",
-                        value = TRUE)
+            selectInput("gender_select", #inputId for the gender selection
+                        "Select Gender", #label for the gender selection selector
+                        choices = c("Male", "Female", "All"), #defined choices; could use from unique(skinny_data$gender), but there is no All -> requires if function in server
+                        selected = "All"), #Part 3, Line 4 asks to display All
+            selectInput("se_show", #inputId for regression error band
+                        "Error Bar Visible", #label for regression error band selector
+                        choices = c("Display Error Band", "Suppress Error Band"), #Part 3, Line 5 defines the two choice options
+                        selected = "Display Error Band"), #Display is the default according to Part 3, Line 5
+            checkboxInput("by_date", #inputId for the date selection
+                        "Include Participants Prior to July 1, 2017", #label for checkbox
+                        value = TRUE #Part 3, Line 6 says to include by default
+            )
         ),
-        mainPanel(
+        mainPanel( # Part 3, Line 3 asks to display the ggplot2 scatterplot; placing in the mainPanel provides best visual
            plotOutput("corPlot")
         )
     )
 )
-
+# This output of the corPlot is defined in the UI
 server <- function(input, output) {
     output$corPlot <- renderPlot({
-      skinny_data <- readRDS("skinny_data.rds")
+      # Part 3, Line 2 asks to import the datafile and slide instructions call for using RDS
+      skinny_data <- readRDS("week8_tbl_skinny.rds") # selects from the same subdirectory as the shiny app
       
+      # Filtering pipeline for if the gender is something other than All (default)
+      if(input$gender_select != "All") {
+        skinny_data <- skinny_data %>% 
+          filter(gender == input$gender_select)
+      }
+      
+      # This variable will define the input of se_show, allowing it to act as TRUE in the geom_smooth(se = )
+      se_display <- input$se_show == "Display Error Band"
+      
+      # Filtering pipeline for if the date checkbox is de-selected
+      if(!input$by_date) {
+        skinny_data <- skinny_data %>% 
+          filter(timeStart >= as.Date("2017-01-01"))
+      }
+      
+      # Plotting function
       ggplot(skinny_data, aes(x = x_mean, y = y_mean)) +
         geom_point(position = "jitter", width = .3) +
-        geom_smooth(method = "lm", se = TRUE, color = "purple")
+        geom_smooth(method = "lm", se = se_display, color = "purple") +
+        xlim(c(1.5,4.75)) + # The limits of the x_mean were obtained by using max() and min() in the console
+        ylim(c(1,4.5)) # The limits of the y_mean were obtained by using max() and min() in the console
     })
 }
 
